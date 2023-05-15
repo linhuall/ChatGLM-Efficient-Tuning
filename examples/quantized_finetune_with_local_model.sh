fb3a327459ed219e60370174dc1906a40db7f156
lin <2334108698@qq.com>
2023-05-16 00:59:02 +0800

    fintuen2.1

--- examples/quantized_finetune_with_local_model.sh
+++ examples/quantized_finetune_with_local_model.sh
@@ -6,7 +6,7 @@ CUDA_VISIBLE_DEVICES=0 python /kaggle/working/model/src/finetune.py \
     --dataset datasetV3 \
     --dataset_dir /kaggle/working/model/data \
     --finetuning_type p_tuning \
-    --output_dir /kaggle/working/checkpoint \
+    --output_dir /kaggle/working/model/checkpoint \
     --overwrite_cache \
     --per_device_train_batch_size 4 \
     --gradient_accumulation_steps 4 \
@@ -14,6 +14,6 @@ CUDA_VISIBLE_DEVICES=0 python /kaggle/working/model/src/finetune.py \
     --logging_steps 10 \
     --save_steps 1000 \
     --learning_rate 5e-5 \
-    --num_train_epochs 1.0 \
+    --num_train_epochs 6.0 \
     --quantization_bit 4 \
     # --fp16 \
